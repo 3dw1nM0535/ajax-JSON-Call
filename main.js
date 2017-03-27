0718f3ae7d7e45833b37a94502c3d935703389e8
3dw1nm0535 <workockmoses@gmail.com>
2017-03-27 15:38:41 +0300

    ajax Call and JSON Data

---
+++ main.js
@@ -0,0 +1,50 @@
+var pageCounter = 1;
+var animalContainer = document.getElementById("animal-info");
+var btn = document.getElementById("btn");
+
+btn.addEventListener("click", function() {
+  var ourRequest = new XMLHttpRequest();
+  ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-' + pageCounter +'.json');
+  ourRequest.onload = function() {
+    if (ourRequest.status >= 200 && ourRequest.status < 400) {
+      var ourData = JSON.parse(ourRequest.responseText);
+      renderHTML(ourData);
+    } else {
+      console.log ("We connected to the server, but returned an Error!");
+    }
+  };
+
+  ourRequest.onerror = function () {
+    console.log ("Connection Error");
+  }
+
+  ourRequest.send();
+  pageCounter++;
+  if (pageCounter > 3) {
+    btn.classList.add("hidden");
+  }
+});
+
+function renderHTML(data) {
+  var htmlString = "";
+  for (var i = 0; i < data.length; i ++ ) {
+    htmlString += "<p>" + data[i].name + " is a " + data[i].species + " that likes to eat ";
+    for (j = 0; j < data[i].foods.likes.length; j++) {
+      if (j == 0) {
+        htmlString += data[i].foods.likes[j];
+      } else {
+        htmlString += " and " + data[i].foods.likes[j];
+      }
+    }
+    htmlString += " and dislikes ";
+    for (j = 0; j < data[i].foods.dislikes.length; j++) {
+      if (j == 0) {
+        htmlString += data[i].foods.dislikes[j];
+      } else {
+        htmlString += " and " + data[i].foods.dislikes[j];
+      }
+    }
+    htmlString += ".</p>";
+  }
+  animalContainer.insertAdjacentHTML("beforeend", htmlString);
+}
